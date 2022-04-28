80ac69dc86402f540735dc1df9e01423a2d47ba3
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-04-27 19:49:51 -0500

    construccion del header de mi pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      Images: {
+      backgroundImage: {
+        'ban1': "url('/public/img/ban1.jpg')",
+        'ban2': "url('/public/img/ban2.png')",
+        'ban3': "url('/public/img/ban3.png')",
+        'ban4': "url('/public/img/ban24.png')",
         'logo': "url('./public/img/log1.png')",
         'me': "url('./public/img/yo.jpg')"
       },
       colors: {
-        'primary': "#f8f8f8",
+        'primary': "#fe5353;",
         'secundary': "#f07684"
       },
       fontFamily: {
